c858ac7a8f8b1094ee565e75b9201a01da74c032
Artem Khimin <a.khimin@yandex.ru>
2023-11-15 19:10:31 +0300

    new dev

---
+++ backtesting.py
@@ -0,0 +1,7 @@
+import numpy as np # библиотека нампи
+import pandas as pd # библиотека пандас
+import matplotlib.pyplot as plt # библиотека матплотлиб для отрисовки
+from IPython.display import clear_output # очистка вывода в ячейке
+import datetime # модуль для работы с datetime
+import warnings # библиотека сообщений по ошибкам
+warnings.filterwarnings("ignore") # игнорировать сообщения ошибок
